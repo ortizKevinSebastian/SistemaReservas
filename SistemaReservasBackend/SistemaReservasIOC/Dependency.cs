9af6d34e79ee539235f09caf9e76962194b3f689
Kevin Inti Sebastián Ortiz <ortizkevinsebastian@gmail.com>
2024-03-07 18:40:34 -0300

    configuracion

---
+++ SistemaReservasBackend/SistemaReservasIOC/Dependency.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaReservasDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaReservasIOC
+{
+    public static class Dependency
+    {
+        public static void DependencyInjection(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<DbReservaContext>(options => {
+                options.UseSqlServer(configuration.GetConnectionString("dbString"));
+            });
+        } 
+    }
+}
